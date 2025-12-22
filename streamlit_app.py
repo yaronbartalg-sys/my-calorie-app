b6a068be55bc5e3c2ee1d378cff54261455b294b
yaronbartalg-sys <yaron.bartal.g@gmail.com>
2025-12-22 14:23:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,4 @@
+GEMINI_API_KEY = "26239ec05cbb01528644f9bd9271d57e"
+
 [connections.gsheets]
 spreadsheet = "https://docs.google.com/spreadsheets/d/1EPVoO5Yx8CV8ZcO_6UdZmO8hwnVpUueB8_y4yEd_33E/edit?usp=sharing"
